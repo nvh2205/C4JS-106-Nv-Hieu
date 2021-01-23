de3dda25a14e532c30211e045d506b63d1cdd48b
quandat10 <quandat3103@gmail.com>
2021-01-23 19:44:45 +0700

    first commit

---
+++ lesson1.js
@@ -0,0 +1,91 @@
+// // let name= prompt("Ho va Ten: ");
+// // let age=prompt("Nhap tuoi: ");
+// // let birth=prompt("Nhap ngay sinh: ");
+// // console.log("Ho va Ten:" + name.toUpperCase());
+// // //console.log(`Ho va Ten: ${name.toUpperCase()}.Tuoi: ${age}`);
+// // //console.log("Tuoi:"+age);
+// // console.log("Ngay sinh:"+birth);
+// let a=Number(prompt("Nhap a:"));
+// let b = typeof(a);
+// console.log(b);
+/*
+
+
+1------Không được bắt đầu bằng số.
+
+Không được chứa những ký tự đặc biệt (khoảng trắng cũng tính là ký tự đặc biệt), ngoại trừ "_" và "$".
+
+Không được trùng với các từ khóa (keywords) của javascript.
+3 ví dụ Invalid variable names:
+  let 4ever;
+  let break;
+  let @gmail.com;
+
+b..(console.log(typeof(x));
+*/
+
+/*
+Câu 2: 1-d,b;
+       2-a;
+       3-e
+       4-c
+
+*/
+
+
+/*Cẩu 3+4:
+
+
+let message="Coding is great";
+console.log(message);
+
+let studentCount=Number(0);
+console.log(studentCount);
+
+message="Coding might not be easy, but stil great";
+console.log(message);
+
+studentCount=16;
+console.log(studentCount);
+
+message = "Coding might not be easy, but still great";
+console.log(message.toLowerCase(message));
+
+studentCount++;
+console.log(studentCount);
+
+*/
+
+/*
+Câu 5:
+alert("You look beautifull today");
+*/
+
+/*Câu 6:
+let name = prompt("Hi there,your name pleaser? ");
+
+alert(`Hi, ${name}.`);
+*/
+
+
+/*Câu 7:
+let firstName = prompt("Enter your first Name?");
+let lastName = prompt("Enter your Last Name ?");
+alert(`Hi, ${lastName} ${firstName}` );
+*/
+
+/*Câu 8:
+ let Acreage=prompt("Enter side length of the square: ");
+ alert(`The square area is ${Acreage*Acreage}`);
+ */
+
+/*Câu 9:
+let Acreage=prompt("Enter the radius of the circle: ");
+alert(`The circle area is ${Acreage*Acreage*3.14}`);
+*/
+
+//Câu 10:
+ let Temperature=Number(prompt("Enter the temperature in Celcius"));
+ alert(`${Temperature}(C) = ${1.8* Temperature +32}(F)`);
+
+
